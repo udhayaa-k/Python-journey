7e758e1c915c5611caadde5f0eac3a9671471f39
udhayaa-k <udhayaa-k@users.noreply.github.com>
2025-12-05 11:20:17 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,17 @@
-git add variables.py
+x = 10
+print(x)
+y = 25
+print(y)
+print("Sum:", x + y)
+print("Diff:", x - y)
+print("Quotient:", x / y)
+print("Remainder:", x % y)
+print("Multiply:", x * y)
+print("Expo:", x ** y)
+#string addition
+a = '1'
+b = '2'
+print("String sum:", a + b)
+#types
+print('type of x is:', type(x))
+print('type of a is:', type(a))
