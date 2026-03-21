3fcfb3e421c34438c396e72a43c632b1bee1dcb0
udhayaa-k <udhayaa-k@users.noreply.github.com>
2026-03-21 12:39:52 +0530

    updated strings.py

---
+++ strings.py
@@ -0,0 +1,13 @@
+name="uDhay"
+print(name.lower())
+print(name.upper())
+print(name.capitalize())
+print(name[0])
+print(name[:1]+"***"+name[-1:])
+song="Shape of you"
+print(song.title())
+#all words first caps
+print(name.replace("D","t"))
+message="your otp is:17645.please keep it safe."
+print(message.split(":")[1].split(".")[0])
+#.strip() to remove space
